9c40614e0bc825d44e386ebe8bce8b22517d390c
ViTran <vitran@teqnological.asia>
2022-06-20 16:59:44 +0700

    fix bug divide 0

--- Sources/PagerTabItemModifier.swift
+++ Sources/PagerTabItemModifier.swift
@@ -21,7 +21,8 @@ struct PagerTabItemModifier<NavTabView: View>: ViewModifier {
                 .onAppear {
                     DispatchQueue.main.async {
                         let frame = reader.frame(in: .named("PagerViewScrollView"))
-                        index = Int(round(frame.minX / frame.width))
+
+                        index = Int(round(frame.minX / frame.width == 0 ? 1 : frame.width))
                         let tabView = navTabView()
                         let tabViewDelegate = tabView as? PagerTabViewDelegate
                         dataStore.setView(AnyView(tabView), at: index)
